d3305e34fbfc6bc9e72161d649b36940074a6ad7
Allysson Santos <68858220+Allyssonantos@users.noreply.github.com>
2024-09-14 15:31:44 -0300

    01

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,4 +1,26 @@
+/* Codigo do menu para celular */
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
+}
+
+
+/**
+ Codigo para o carousel
+ */
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const items = document.querySelectorAll('.carousel-item');
+    const totalItems = items.length;
+
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalItems - 1;
+    } else if (currentIndex >= totalItems) {
+        currentIndex = 0;
+    }
+
+    document.querySelector('.carousel-inner').style.transform = `translateX(${-currentIndex * 100}%)`;
 }
